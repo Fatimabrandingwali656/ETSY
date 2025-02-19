f4cbe98f1d5abc8a3c7d488df5fd12639eb7067f
Fatimabrandingwali656 <zarwafatima27@gmail.com>
2025-02-19 16:45:21 +0500

    Create login.php

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php include 'db.php'; session_start();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $result = $conn->query("SELECT * FROM users WHERE email='$email'");
+    $user = $result->fetch_assoc();
+
+    if ($user && password_verify($password, $user['password'])) {
+        $_SESSION['user_id'] = $user['id'];
+        header("Location: index.php");
+    } else {
+        echo "Invalid login.";
+    }
+}
+?>
+
+<form method="post">
+    Email: <input type="email" name="email" required><br>
+    Password: <input type="password" name="password" required><br>
+    <button type="submit">Login</button>
+</form>
